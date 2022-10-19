df9ffa774cdeba1aa2cc76b5d14a9d33253f1241
suzuki-yud <suzuki.yud1028@gmail.com>
2022-10-19 14:15:24 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,56 @@
+import * as React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { createColumnHelper } from "@tanstack/react-table";
+import { DataTable } from "./components/DataTable";
+
+type UnitConversion = {
+  fromUnit: string;
+  toUnit: string;
+  factor: number;
+};
+
+const data: UnitConversion[] = [
+  {
+    fromUnit: "inches",
+    toUnit: "millimetres (mm)",
+    factor: 25.4
+  },
+  {
+    fromUnit: "feet",
+    toUnit: "centimetres (cm)",
+    factor: 30.48
+  },
+  {
+    fromUnit: "yards",
+    toUnit: "metres (m)",
+    factor: 0.91444
+  }
+];
+
+const columnHelper = createColumnHelper<UnitConversion>();
+
+const columns = [
+  columnHelper.accessor("fromUnit", {
+    cell: (info) => info.getValue(),
+    header: "To convert"
+  }),
+  columnHelper.accessor("toUnit", {
+    cell: (info) => info.getValue(),
+    header: "Into"
+  }),
+  columnHelper.accessor("factor", {
+    cell: (info) => info.getValue(),
+    header: "Multiply by",
+    meta: {
+      isNumeric: true
+    }
+  })
+];
+
+export default function App() {
+  return (
+    <ChakraProvider>
+      <DataTable columns={columns} data={data} />
+    </ChakraProvider>
+  );
+}
